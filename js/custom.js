12abf9f25870efac699917190b8cb73b94c43c64
Ayush <ayushxx7@gmail.com>
2020-08-24 22:25:23 +0530

    fix: hyperlink not changing. update: tools
    The hyperlink should cycle through a list of predefined links. The issue
    was related to `"` inside `"`. Changed the outer " to ' to fix.

--- js/custom.js
+++ js/custom.js
@@ -12,140 +12,131 @@
 // });
 
 // function typeWriter() {
-  
+
 // }
 //s
 
 
- var text = ["<span style="color: red"><a href='https://github.com/ayushxx7'>I'm Here</a></span>", "<a href='https://facebook.com/ayush.mandowara'>I'm Also Here</a>", "<a href='https://www.twitter.com/ayushxx7'>And Here</a>","<a href=https://www.google.co.in/search?q=ayushxx7>Because, Why Not?</a>"];
- //var color = ["red","green","orange"]   
-    var counter = 0;
-    var elem = document.getElementById("here");
-    counter_color = ["red","green","blue","white"];
-    
-    function change() {    
-     //elem.className += " colorlink";   
-     //elem.style.color = color[counter];
-     elem.innerHTML = text[counter];
-      elem.style.color = counter_color[counter];
-        counter++;
-        if(counter >= text.length) { counter = 0; }
-    }
-    setInterval(change, 5000);
- 
-$('#vr').on('click', function(event) {
-  window.open("https://drive.google.com/file/d/0ByGd2xHHajBgTHJKR3U4ZldhN2s/view?usp=sharing","_blank");    
-});    
+var text = ["<span style='color: red'><a href='https://github.com/ayushxx7'>I'm Here</a></span>", "<a href='https://facebook.com/ayush.mandowara'>I'm Also Here</a>", "<a href='https://www.twitter.com/ayushxx7'>And Here</a>", "<a href=https://www.google.co.in/search?q=ayushxx7>Because, Why Not?</a>"];
+var counter = 0;
+var elem = document.getElementById("here");
+counter_color = ["red", "green", "blue", "white"];
 
-// $('#dr').on('click',function(event){
-//     event.preventDefault();
-//     window.location = '\img\\header-bg.jpg';
-// });
-$("#af").click(function(){
-    $("#awhat").toggle();
-    $("#ahow").toggle();
+function change() {
+  //elem.className += " colorlink";   
+  //elem.style.color = color[counter];
+  elem.innerHTML = text[counter];
+  elem.style.color = counter_color[counter];
+  counter++;
+  if (counter >= text.length) {counter = 0;}
+}
+setInterval(change, 2500);
+
+$('#vr').on('click', function (event) {
+  window.open("https://drive.google.com/file/d/0ByGd2xHHajBgTHJKR3U4ZldhN2s/view?usp=sharing", "_blank");
 });
 
-$("#awhat").click(function(){
-    $("#awhat1").toggle();
+$("#af").click(function () {
+  $("#awhat").toggle();
+  $("#ahow").toggle();
 });
 
-$("#ahow").click(function(){
-    $("#ahow1").toggle();
+$("#awhat").click(function () {
+  $("#awhat1").toggle();
+});
+
+$("#ahow").click(function () {
+  $("#ahow1").toggle();
 });
 
 
-$("#fa").click(function(){
-    $("#fwhat").toggle();
-    $("#fhow").toggle();
+$("#fa").click(function () {
+  $("#fwhat").toggle();
+  $("#fhow").toggle();
 });
 
-$("#fwhat").click(function(){
-    $("#fwhat1").toggle();
+$("#fwhat").click(function () {
+  $("#fwhat1").toggle();
 });
 
-$("#fhow").click(function(){
-    $("#fhow1").toggle();
-});
-
-
-$("#ww").click(function(){
-    $("#cm").toggle();
-    $("#ad").toggle();
-    $("#adt").toggle();
-
-});
-
-$("#cm").click(function(){
-    $("#cwhat").toggle();
-    $("#cwhy").toggle();
-    $("#chow").toggle();
-    //$("#cwhat1").hide();
+$("#fhow").click(function () {
+  $("#fhow1").toggle();
 });
 
 
-$("#cwhat").click(function(){
-    $("#cwhat1").toggle();
+$("#ww").click(function () {
+  $("#cm").toggle();
+  $("#ad").toggle();
+  $("#adt").toggle();
 });
 
-$("#chow").click(function(){
-    $("#chow1").toggle();
-});
-
-$("#cwhy").click(function(){
-    $("#cwhy1").toggle();
-});
-
-$("#ad").click(function(){
-    $("#adwhat").toggle();
-    $("#adwhy").toggle();;
+$("#cm").click(function () {
+  $("#cwhat").toggle();
+  $("#cwhy").toggle();
+  $("#chow").toggle();
 });
 
 
-$("#adwhat").click(function(){
-    $("#adwhat1").toggle();
+$("#cwhat").click(function () {
+  $("#cwhat1").toggle();
+});
+
+$("#chow").click(function () {
+  $("#chow1").toggle();
+});
+
+$("#cwhy").click(function () {
+  $("#cwhy1").toggle();
+});
+
+$("#ad").click(function () {
+  $("#adwhat").toggle();
+  $("#adwhy").toggle();;
 });
 
 
-$("#adwhy").click(function(){
-    $("#adwhy1").toggle();
+$("#adwhat").click(function () {
+  $("#adwhat1").toggle();
 });
 
 
-$("#adt").click(function(){
-    $("#adtwhat").toggle();
-    $("#adtwhy").toggle();
-    $("#adthow").toggle();
-    //$("#cwhat1").hide();
+$("#adwhy").click(function () {
+  $("#adwhy1").toggle();
 });
 
 
-$("#adtwhat").click(function(){
-    $("#adtwhat1").toggle();
-});
-
-$("#adthow").click(function(){
-    $("#adthow1").toggle();
-});
-
-$("#adtwhy").click(function(){
-    $("#adtwhy1").toggle();
+$("#adt").click(function () {
+  $("#adtwhat").toggle();
+  $("#adtwhy").toggle();
+  $("#adthow").toggle();
 });
 
 
-$("#zl").click(function(){
-    $("#zlwhat").toggle();
-    $("#zlhow").toggle();
-    //$("#cwhat1").hide();
+$("#adtwhat").click(function () {
+  $("#adtwhat1").toggle();
+});
+
+$("#adthow").click(function () {
+  $("#adthow1").toggle();
+});
+
+$("#adtwhy").click(function () {
+  $("#adtwhy1").toggle();
 });
 
 
-$("#zlwhat").click(function(){
-    $("#zlwhat1").toggle();
+$("#zl").click(function () {
+  $("#zlwhat").toggle();
+  $("#zlhow").toggle();
 });
 
-$("#zlhow").click(function(){
-    $("#zlhow1").toggle();
+
+$("#zlwhat").click(function () {
+  $("#zlwhat1").toggle();
+});
+
+$("#zlhow").click(function () {
+  $("#zlhow1").toggle();
 });
 
 
